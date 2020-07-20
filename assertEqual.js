3a437f4804a604309fef7b141dcb4870469624f6
Marisa Doig <mcr.doig@gmail.com>
2020-07-20 22:44:09 +0000

    Created function to log whether or not input is equivalent

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(":relieved::relieved::relieved: Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(":weary::weary::weary: Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Cool", "Cool");
+assertEqual(17, 97);
