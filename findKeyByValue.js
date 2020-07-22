d50f1ca4f863df24af708af72b283a1987b37f17
Marisa Doig <mcr.doig@gmail.com>
2020-07-22 21:54:07 +0000

    Iterates over an object to find the key that matches inputted value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😌😌😌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔😔😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key of Object.keys(object)) {
+    //console.log(`key: ${key}, value: ${value}`);
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
