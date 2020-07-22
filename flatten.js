c4913abb04f8c83093c36e1753144459931df196
Marisa Doig <mcr.doig@gmail.com>
2020-07-22 05:58:57 +0000

    read in an array and if an element is an array, bring the values into main array

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  let areEqual = true;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      areEqual = false;
+    }
+  }
+  return areEqual;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`${array1} === ${array2}\nThese arrays are equal :)`);
+  } else {
+    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`);
+  }
+};
+
+const flatten = function(array){
+  let flattenedArray = [];
+  
+  for(let element of array){
+    if(Array.isArray(element)){
+      for(let item of element){
+        flattenedArray.push(item);
+      }
+    }else{
+      flattenedArray.push(element);
+    }
+  }
+
+  return flattenedArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
