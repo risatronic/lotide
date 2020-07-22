27d513a202670326910296f9ec8b530bc0d28d75
Marisa Doig <mcr.doig@gmail.com>
2020-07-22 23:24:21 +0000

    Cleaned up to use for...in instead of for...let with Object.keys

--- eqObjects.js
+++ eqObjects.js
@@ -21,12 +21,11 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  for (let key of Object.keys(object1)) {
+  for (let key in object1) {
     if (object1[key].length !== object2[key].length) {
       return false;
     }
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      //console.log("ARRAY");
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
