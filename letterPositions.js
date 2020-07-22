280503b5b8b97a1cc98562593daa6b46bf9148a2
Marisa Doig <mcr.doig@gmail.com>
2020-07-22 21:34:43 +0000

    A function to loop through a string and return an object containing letter keys with arrays of the position(s) of that letter as values

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  let areEqual = true;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      areEqual = false;
+    }
+  }
+  return areEqual;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`${array1} === ${array2}\nThese arrays are equal :)`);
+  } else {
+    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`);
+  }
+};
+
+const letterPositions = function(string) {
+  const results = {};
+  let i = 0;
+
+  for (let char of string) {
+    if (char !== " ") {
+      if (!results[char]) {
+        results[char] = [i];
+      } else {
+        results[char].push(i);
+      }
+    }
+    i++;
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
