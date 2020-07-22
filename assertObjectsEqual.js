e08186d1bce597ed368bd061534bc5d9a3195551
Marisa Doig <mcr.doig@gmail.com>
2020-07-22 23:37:05 +0000

    A function that uses eqArrays and eqObjects functions to take in 2 objects and print whether or not they are equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,54 @@
+const eqArrays = function(array1, array2) {
+  let areEqual = true;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      areEqual = false;
+    }
+  }
+  return areEqual;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (object1[key].length !== object2[key].length) {
+      return false;
+    }
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (!object1[key] === object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  //console.log(`Example label: ${inspect(object1)}`)
+
+  if (eqObjects(object1, object2)) {
+    console.log(`${inspect(object1)} === ${inspect(object2)}\nThese objects are equal 😸\n`);
+  } else {
+    console.log(`${inspect(object1)} !== ${inspect(object2)}\nThese objects are not equal 😭\n`);
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
