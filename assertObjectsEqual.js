81bfe2c12728df469170f79d7105ba1ad4c1fc26
Marisa Doig <mcr.doig@gmail.com>
2020-07-23 23:05:08 +0000

    Improved eqArrays function to compare array length rather than just values after noticing it could return true if array1 shorter than array2, also updated any other files that use it

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -2,7 +2,7 @@ const eqArrays = function(array1, array2) {
   let areEqual = true;
 
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
       areEqual = false;
     }
   }
