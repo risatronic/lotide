be33ca39f343b35cf949d68cebae48ef36fbec67
Marisa Doig <mcr.doig@gmail.com>
2020-07-28 03:45:38 +0000

    removed tests to separate files

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😌😌😌 Assertion Passed: ${actual} === ${expected}`);
@@ -7,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Cool", "Cool");
-assertEqual(17, 97);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`:relieved::relieved::relieved: Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`:weary::weary::weary: Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 const head = function(array){
   return array[0];
 }
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello"]), "Hello");
+module.exports = head;
