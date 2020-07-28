d2c722159d21f08a11fbebf4a41d0ea5df56c7fb
Marisa Doig <mcr.doig@gmail.com>
2020-07-28 04:16:07 +0000

    moved tests to external file

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let areEqual = true;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      areEqual = false;
-    }
-  }
-  return areEqual;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`${array1} === ${array2}\nThese arrays are equal :)`);
-  } else {
-    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`);
-  }
-};
-
 const middle = function(array) {
   const returnArray = [];
 
@@ -34,9 +15,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
